b99cfb242949d829b71539844d0782a591c2865f
John Wason <wason@wasontech.com>
2019-06-16 01:33:52 -0400

    Initial commit

---
+++ Formula/robotraconteur.rb
@@ -0,0 +1,20 @@
+class Robotraconteur < Formula
+  desc "Robot Raconteur: communication framework for robotics"
+  homepage "https://robotraconteur.com"
+  url "https://github.com/robotraconteur/robotraconteur/archive/v0.9.0.tar.gz"
+  sha256 "9870fa9bdcd305c664ad41de508adf6a4be3cf329a825bf655aa43bb8f00f68d"
+  head "https://github.com/robotraconteur/robotraconteur.git"
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "openssl"
+
+  def install
+    system "cmake", ".", "-DCMAKE_CXX_STANDARD=11", "-DBUILD_GEN=ON", "-DBoost_USE_STATIC_LIBS=ON",
+           "-DOPENSSL_USE_STATIC_LIBS=ON", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "ctest", "-C", "Release"
+  end
+end
