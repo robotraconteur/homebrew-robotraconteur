1eeb2ba18fcee5933c9908a451927f5195ce960b
John Wason <wason@wasontech.com>
2021-03-19 18:12:10 -0400

    Update to v0.15.1

--- Formula/robotraconteur.rb
+++ Formula/robotraconteur.rb
@@ -1,8 +1,8 @@
 class Robotraconteur < Formula
   desc "Robot Raconteur: communication framework for robotics"
   homepage "https://robotraconteur.com"
-  url "https://github.com/robotraconteur/robotraconteur/archive/v0.15.0.tar.gz"
-  sha256 "499fc4443e7ebbd764ad19d68ca61e29cb337f7fe34708ef00311c49d664acee"
+  url "https://github.com/robotraconteur/robotraconteur/archive/v0.15.1.tar.gz"
+  sha256 "2fd45f319e33162f04cbc946edf96aea3eff736744b66c8c9f17817c7667348e"
   head "https://github.com/robotraconteur/robotraconteur.git"
   depends_on "cmake" => :build
   depends_on "boost"
